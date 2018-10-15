733a6bc4bf91752222648f0aa707fe8dd64487ae
Ji S <j2yshin@gmail.com>
2018-10-15 19:19:25 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,19 @@
 def instantiate_new_array
   array = []
 end
+
+def array_with_two_elements
+  array = ["what's", "up"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
